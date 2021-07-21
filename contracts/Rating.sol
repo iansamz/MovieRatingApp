db954496cf00b46284b7cda48c7f321ae37e15f7
Ian Mungai <iansam74@gmail.com>
2021-07-21 09:21:39 +0300

    clontract change

--- contracts/Rating.sol
+++ contracts/Rating.sol
@@ -1,33 +1,31 @@
-//pragma solidity ^0.5.8;
-pragma experimental ABIEncoderV2;
-// We have to specify what version of compiler this code will compile with
+pragma solidity >=0.7.0 <0.9.0;
 
 contract Rating {
-  /* mapping field below is equivalent to an associative array or hash.
-  */
-  
-  mapping (string => uint8) public ratingsReceived;
-  
-  /* We will use an array of bytes32 to store the list of movies
-  */
-  
-  string[] public movieList;
+    /* mapping field below is equivalent to an associative array or hash.
+     */
 
-  /* This is the constructor which will be called once when you
+    mapping(string => uint8) public ratingsReceived;
+
+    /* We will use an array of bytes32 to store the list of movies
+     */
+
+    string[] public movieList;
+
+    /* This is the constructor which will be called once when you
   deploy the contract to the blockchain. When we deploy the contract,
   we will pass an array of movies for which users will give ratings
   */
-  constructor(string[] memory movieNames) public {
-    movieList = movieNames;
-  }
+    constructor(string[] memory movieNames) public {
+        movieList = movieNames;
+    }
 
-  // This function returns the total ratings a movie has received so far
-  function totalVotesFor(string memory movie) view public returns (uint8) {
-    return ratingsReceived[movie];
-  }
+    // This function returns the total ratings a movie has received so far
+    function totalVotesFor(string memory movie) public view returns (uint8) {
+        return ratingsReceived[movie];
+    }
 
-  // This function increments the vote count for the specified movie. Equivalent to upvoting
-  function voteForMovie(string memory movie) public {
-    ratingsReceived[movie] += 1;
-  }
+    // This function increments the vote count for the specified movie. Equivalent to upvoting
+    function voteForMovie(string memory movie) public {
+        ratingsReceived[movie] += 1;
+    }
 }
