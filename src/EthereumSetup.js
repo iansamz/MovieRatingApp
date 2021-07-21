77d358cf53b59d988a76eaf62aa247590e0e6659
Ian Mungai <iansam74@gmail.com>
2021-07-21 11:41:41 +0300

    eth

--- src/EthereumSetup.js
+++ src/EthereumSetup.js
@@ -1,11 +1,11 @@
 import Web3 from 'web3';
 
-const web3=new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 //const web3=new Web3(Web3.currentProvider);
-let ratingABI=[{"constant":true,"inputs":[{"name":"","type":"string"}],"name":"ratingsReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"movieList","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"movieNames","type":"string[]"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"constant":true,"inputs":[{"name":"movie","type":"string"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"movie","type":"string"}],"name":"voteForMovie","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}]
-let ratingAddress='0x2d3798616746E3a6d95f78cb4d6B988cD8f25ccf';
+let ratingABI = [{ "constant": true, "inputs": [{ "name": "", "type": "string" }], "name": "ratingsReceived", "outputs": [{ "name": "", "type": "uint8" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [{ "name": "", "type": "uint256" }], "name": "movieList", "outputs": [{ "name": "", "type": "string" }], "payable": false, "stateMutability": "view", "type": "function" }, { "inputs": [{ "name": "movieNames", "type": "string[]" }], "payable": false, "stateMutability": "nonpayable", "type": "constructor" }, { "constant": true, "inputs": [{ "name": "movie", "type": "string" }], "name": "totalVotesFor", "outputs": [{ "name": "", "type": "uint8" }], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [{ "name": "movie", "type": "string" }], "name": "voteForMovie", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }]
+let ratingAddress = '0x2d3798616746E3a6d95f78cb4d6B988cD8f25ccf';
 web3.eth.defaultAccount = web3.eth.accounts[0]
 
 
-const ratingContract=web3.eth.contract(ratingABI).at(ratingAddress);
-export {ratingContract};
+const ratingContract = web3.eth.contract(ratingABI).at(ratingAddress);
+export { ratingContract };
